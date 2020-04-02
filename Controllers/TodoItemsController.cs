f6d6845f0ea1474eae1c329307cc7147b839d7ed
CassandraDeProspero <deprospero.2@osu.edu>
2020-04-02 13:01:51 -0400

    commit

---
+++ Controllers/TodoItemsController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Todo.Api.Models;
+using Todo.Api.Services;
+
+namespace Todo.Api.Controllers
+{
+    [ApiController]
+    [Route("api/todos")]
+    public class TodoItemsController : ControllerBase
+    {
+          private TodoRepository repository;
+        public TodoItemsController(ITodoRepository todoRepository)
+        {
+            repository = todoRepository ??
+                throw new ArgumentNullException(nameof(todoRepository));
+        }
+        [HttpGet]
+        public IActionResult GetTodos()
+        {
+            TodoItem todoItem = new TodoItem { Id = 2, Action = "Thing two.", IsDone = false };
+            var result = repository.GetAllTodoItems();
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        private IActionResult ok(List<TodoItem> result)
+        {
+            throw new NotImplementedException();
+        }
+      
+    }
+}
